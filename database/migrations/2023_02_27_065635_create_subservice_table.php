abf31ca7d36fb6b813cfca8e89d52e820adb2238
Azizjon <azizjon.allanazarov@ssd.uz>
2023-02-27 12:50:34 +0500

    Create migrations

---
+++ database/migrations/2023_02_27_065635_create_subservice_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subservice', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('service_id');
+
+            $table->string('name_ru', 255)->nullable();
+            $table->string('name_uz', 255)->nullable();
+            $table->string('name_en', 255)->nullable();
+
+            $table->text('content_ru')->nullable();
+            $table->text('content_uz')->nullable();
+            $table->text('content_en')->nullable();
+
+            $table->foreign('service_id')->references('id')->on('service')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subservice', function (Blueprint $table) {
+            $table->dropForeign('subservice_service_id_foreign');
+            $table->dropIfExists();
+        });
+    }
+};
